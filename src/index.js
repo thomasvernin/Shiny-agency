67fce9654daed5c2267f0a8bd150de766884d86d
Thomas Vernin <tvernin49100@gmail.com>
2024-05-28 15:24:43 +0200

    Styled components

--- src/index.js
+++ src/index.js
@@ -7,10 +7,22 @@ import Results from './pages/Results'
 import Freelances from './pages/Freelances'
 import Header from './components/Header'
 import Error from './components/Error'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+
+    body {
+      margin: 0;
+    }
+`
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <GlobalStyle />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
